a8530d7f4863c1fd13d9a26f9b4d61d8d5853e25
Sinsohi <sohi8451@naver.com>
2024-03-13 11:29:10 +0900

    5강 - 버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+/* eslint-disable */ // -> lint 끄는 기능
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
 function App() {
 
-  let [글제목,b] = useState('여자 코트 추천');
+  let [글제목,setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   // a -> state에 보관했던 자료 나옴
   // b -> state 변경 도와주는 함수
   // 변수와 state의 차이 : state 쓰던 html은 자동 재렌더링 됨
 
   let [logo,setLogo] = useState('ReactBlog');
-  let [title2, setTitle_2] = useState('강남 우동 맛집');
-  let [title3,setTitle_3] = useState('파이썬 독학');
+  let [따봉,따봉변경] = useState(0)
 
+  
 
   
   return (
@@ -21,15 +22,18 @@ function App() {
         <h4>{logo}</h4>
       </div>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉++)}}>👍</span> {따봉} </h4>
+        <p>2월 17일 발행<span onClick={()=>{setTitle(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학'])}}> 👗</span></p>
+      </div>
+
+
+
+      <div className="list">
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{title2}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title3}</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
       
