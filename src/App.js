476548e73af470505f51ed0a2c7a0729d0258e7d
Sinsohi <sohi8451@naver.com>
2024-03-19 19:02:49 +0900

    20강 - 리액트 라우터 1 : 셋팅이랑 기본 라우팅

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Navbar, Nav  } from 'react-bootstrap';
 import data from './data.js';
+import { Routes, Route, Link, RouterProvider} from 'react-router-dom'
+import Detail from './detail.js'
 
 
 
@@ -12,21 +14,30 @@ function App() {
   let [shoes,setShoes] = useState(data)
   return (
     <div className="App">
+
+
+
       {/* Navbar */}
      <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
+          
+          {/* 페이지 이동 버튼 */}
+           <Link to="/"> Home</Link>
+           <Link to="/detail">Detail</Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
       <br />
 
-    <div class="main-bg"></div>
 
+
+         {/* 라우터로 페이지 나누는 법 */}
+         <Routes>
+        <Route path='/' element={<>
+          <div class="main-bg"></div>
    <div className='container'>
       <div className='row'>
         {
@@ -39,9 +50,12 @@ function App() {
           )
         }
       </div>
-
-
    </div>
+   </>}/> 
+        <Route path='/detail' element={<Detail></Detail>}/> 
+      </Routes>
+
+ 
 
 </div>
   )
