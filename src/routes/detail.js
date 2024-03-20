7fd28f740b03526a9c255fb9f44064aa45252f79
Sinsohi <sohi8451@naver.com>
2024-03-21 01:50:25 +0900

    23강 - styled-components 쓰면 CSS 파일 없어도 되는데

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,4 +1,21 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+let YellowBtn = styled.button`
+  background : ${props => props.bg};
+  color : ${ props => props.bg=='blue' ? 'white' : 'black'};
+  padding : 10px;
+`
+
+let BlackBox = styled.div`
+  background : grey;
+  padding : 20px;
+`
+
+// 기존 스타일 복사 가능
+let NewBtn = styled.button(YellowBtn)` 
+  background : red;
+`
 
 function Detail(props){
 
@@ -9,6 +26,12 @@ function Detail(props){
 
    return(
     <div className="container">
+      <BlackBox>
+      <YellowBtn bg="blue">버튼</YellowBtn>   
+      <YellowBtn bg="pink">버튼</YellowBtn>
+
+      </BlackBox>
+
     <div className="row">
       <div className="col-md-6">
         <img src={'https://codingapple1.github.io/shop/shoes' + (parseInt(id)+1) +'.jpg'} width="100%" />
