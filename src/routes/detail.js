e056c132aac0d8b79f92722a9f2750af86ef11d8
Sinsohi <sohi8451@naver.com>
2024-03-20 12:11:08 +0900

    22강 - 리액트 라우터 3 : URL 파라미터로 상세페이지 100개 만들기

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,14 +1,23 @@
-function Detail(){
+import { useParams } from "react-router-dom";
+
+function Detail(props){
+
+  let {id} = useParams(); // 유저가 URL 파라미터에 입력한거 가져오려면 useParams()
+  let 찾은상품 = props.shoes.find(function(x){
+    return x.id == id
+  })
+
    return(
     <div className="container">
     <div className="row">
       <div className="col-md-6">
-        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        <img src={'https://codingapple1.github.io/shop/shoes' + (parseInt(id)+1) +'.jpg'} width="100%" />
+        {console.log(typeof(id))}
       </div>
       <div className="col-md-6">
-        <h4 className="pt-5">상품명</h4>
-        <p>상품설명</p>
-        <p>120000원</p>
+        <h4 className="pt-5">{찾은상품.title}</h4>
+        <p>{찾은상품.content}</p>
+        <p>{찾은상품.price}원</p>
         <button className="btn btn-danger">주문하기</button> 
       </div>
     </div>
