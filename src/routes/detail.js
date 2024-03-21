8995dd582c046fd96d7873208a53032876ea1c20
Sinsohi <sohi8451@naver.com>
2024-03-21 13:24:00 +0900

    24강 - Lifecycle과 useEffect 1

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,41 +1,39 @@
 import { useParams } from "react-router-dom";
-import styled from 'styled-components';
+import { useEffect} from 'react';
+import {useState} from 'react';
 
-let YellowBtn = styled.button`
-  background : ${props => props.bg};
-  color : ${ props => props.bg=='blue' ? 'white' : 'black'};
-  padding : 10px;
-`
-
-let BlackBox = styled.div`
-  background : grey;
-  padding : 20px;
-`
-
-// 기존 스타일 복사 가능
-let NewBtn = styled.button(YellowBtn)` 
-  background : red;
-`
 
 function Detail(props){
 
-  let {id} = useParams(); // 유저가 URL 파라미터에 입력한거 가져오려면 useParams()
+// 컴포넌트에 갈고리 다는 법
+  useEffect(()=>{
+    // mount, update시 여기 코드 실행됨 
+    console.log('안녕');
+    // (useEffect 안에 있는 코드는 html 렌더링 후에 동작) -> 어려운 연산, 서버에서 데이터 가져오는 작업
+
+    setTimeout(()=>{
+      document.querySelector('.alert').style.display = 'none';
+    },2000)
+    })
+
+  let [count, setCount] = useState(0);
+
+  let {id} = useParams(); 
   let 찾은상품 = props.shoes.find(function(x){
     return x.id == id
   })
 
    return(
     <div className="container">
-      <BlackBox>
-      <YellowBtn bg="blue">버튼</YellowBtn>   
-      <YellowBtn bg="pink">버튼</YellowBtn>
-
-      </BlackBox>
-
+      <div className="alert alert-warning">
+        2초 이내 구매시 할인
+      </div>
+      
+      {count}
+      <button onClick={()=>{setCount(count+1)}}>버튼</button>
     <div className="row">
       <div className="col-md-6">
         <img src={'https://codingapple1.github.io/shop/shoes' + (parseInt(id)+1) +'.jpg'} width="100%" />
-        {console.log(typeof(id))}
       </div>
       <div className="col-md-6">
         <h4 className="pt-5">{찾은상품.title}</h4>
