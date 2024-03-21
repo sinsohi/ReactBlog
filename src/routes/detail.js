ff2df5f6e99ba1e0abca356021ab9a71b1463d9d
Sinsohi <sohi8451@naver.com>
2024-03-21 19:04:40 +0900

    25강 - Lifecycle과 useEffect 2

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -5,37 +5,61 @@ import {useState} from 'react';
 
 function Detail(props){
 
-// 컴포넌트에 갈고리 다는 법
-  useEffect(()=>{
-    // mount, update시 여기 코드 실행됨 
-    console.log('안녕');
-    // (useEffect 안에 있는 코드는 html 렌더링 후에 동작) -> 어려운 연산, 서버에서 데이터 가져오는 작업
-
-    setTimeout(()=>{
-      document.querySelector('.alert').style.display = 'none';
-    },2000)
-    })
 
   let [count, setCount] = useState(0);
-
+  let [alert, setAlert] = useState(true);
   let {id} = useParams(); 
   let 찾은상품 = props.shoes.find(function(x){
     return x.id == id
-  })
+  });
+  let [num,setNum] = useState(''); 
+  let [warning, setWarning] = useState(false); 
+
+  useEffect(()=>{
+    if (isNaN(num) == true){
+      setWarning(true);
+    }
+    else{
+      setWarning(false);
+    }
+  }, [num])
+
+  useEffect(()=>{
+    let Timer = setTimeout(()=>{setAlert(false)},2000)
+      console.log(2)
+      return ()=>{
+        console.log(1);
+        clearTimeout(Timer);
+      } // useEffect 동작 전에 실행됨
+   },[]) // 컴포넌트 mount시 1회만 실행하고 싶으면 이렇게
+   // 특정 state 변경시에만 실행하려면 [state명]
 
    return(
+
     <div className="container">
-      <div className="alert alert-warning">
-        2초 이내 구매시 할인
-      </div>
-      
+    <div className="row">
+      {
+      alert == true 
+      ? <div className="alert alert-warning">
+          2초 이내 구매시 할인
+        </div>
+         : null
+      }
+
       {count}
       <button onClick={()=>{setCount(count+1)}}>버튼</button>
-    <div className="row">
       <div className="col-md-6">
         <img src={'https://codingapple1.github.io/shop/shoes' + (parseInt(id)+1) +'.jpg'} width="100%" />
       </div>
       <div className="col-md-6">
+
+        {
+          warning == true ? <div>그러지마세요</div> : null
+        }
+
+        <input onChange={(e)=>{
+          setNum(e.target.value)
+        }}></input>
         <h4 className="pt-5">{찾은상품.title}</h4>
         <p>{찾은상품.content}</p>
         <p>{찾은상품.price}원</p>
